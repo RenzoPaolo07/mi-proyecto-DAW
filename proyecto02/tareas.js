afa024c4311da2b529765a61c59aeaef9f2ff565
Renzo Costa <60995421@continental.edu.pe>
2026-05-29 15:35:04 -0500

    Commit desde GitHub proyecto02

--- proyecto02/tareas.js
+++ proyecto02/tareas.js
@@ -2,3 +2,6 @@ console.log("Lista de tareas");
 console.log("Agregar tarea 1");
 console.log("Agregar tarea 2");
 console.log("Agregar tarea 3");
+console.log("Agregar tarea 4");
+console.log("Agregar tarea 5");
+console.log("Agregar tarea 6");
